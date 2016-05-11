c3def063ac3726a5fa52c6f58d9d4aefb9af6cc8
Scott Morris <smorris@opsline.com>
2016-05-11 18:47:29 -0400

    support 'url' as a vpn firewall config key and do ip lookups on the url

--- recipes/firewall.rb
+++ recipes/firewall.rb
@@ -1,7 +1,7 @@
 #
 # Sample of multidaemon config hash. This should normally go in your vpn server role.
 # Valid keys in the 'destinations' hash are role, environment, ip (specific ip, ip range or CIDR), protocol and port
-# You must include either the 'ip' key OR use 'role' and/or 'environment' keys to lookup the valid ips
+# You must include either the 'ip' key, 'url' key OR use 'role' and/or 'environment' keys to lookup the valid ips
 # If you want to specify 'protocol' or 'port', you must use both together
 #
 # 'opsline-openvpn' => {
@@ -84,8 +84,23 @@ node['opsline-openvpn']['daemons'].each do |k,v|
         query = nil # no need to run chef search query
         rule['ip'] = d['ip']
         rules << rule.dup
-        log "creating firewall rule to #{permission} traffic from #{source_cidr} to #{ip}#{port_msg}"
-      else # need to lookup the ip(s) using chef search
+        log "creating firewall rule to #{permission} traffic from #{source_cidr} to #{rule['ip']}#{port_msg}"
+      
+      elsif d.has_key?('url')
+        query = nil # no need to run chef search query
+        ips = []
+        log "resolve url #{d['url']} to private ip address(es) for firewall rule"
+        require 'resolv'
+        ips = Resolv::DNS.new.getaddresses('admin.getbread.com').map(&:to_s).sort
+        
+        ips.each do |ip|
+          rule['ip'] = ip
+          rules << rule.dup
+          log "creating firewall rule to #{permission} traffic from #{source_cidr} to #{rule['ip']}#{port_msg}"
+        end
+
+      else
+        # need to lookup the ip(s) using chef search
         if d.has_key?('role')
           query += "roles:#{d['role']}"
         end
@@ -109,17 +124,22 @@ node['opsline-openvpn']['daemons'].each do |k,v|
 
     log "rules: #{rules}"
 
-    iptables_rule "openvpn_#{k}_firewall" do
-      source 'firewall_rules.erb'
-      variables({
-        :rules => rules,
-        :source_cidr => source_cidr,
-        :permission => permission,
-        :inverse_permission => inverse_permission
-      })
-    end
+    rules_action = :enable
+
   else
-    log "Invalid firewall specification for vpn daemon: #{k}"
+    log "No valid firewall specification for vpn daemon: #{k}"
+    rules_action = :disable
+  end
+
+  iptables_rule "openvpn_#{k}_firewall" do
+    source 'firewall_rules.erb'
+    variables({
+      :rules => rules,
+      :source_cidr => source_cidr,
+      :permission => permission,
+      :inverse_permission => inverse_permission
+    })
+    action rules_action
   end
 end
 
