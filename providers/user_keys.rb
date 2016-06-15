51b9f5d8a1d03e0239429646758b98828949d009
Scott Morris <smorris@opsline.com>
2016-06-15 17:18:08 -0400

    fix for removing peresisted keys and databag items for a deleted user

--- providers/user_keys.rb
+++ providers/user_keys.rb
@@ -64,57 +64,60 @@ action :create do
     rescue StandardError => e
       log "Caught exception #{e} while searching for persisted user keys in #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item}"
       log "Persisted openvpn user keys do not exist in #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} databag item"
-      log "New openvpn user keys will be generated and saved to #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} databag item"
-      persisted_certs = nil
 
-      # generate new user keys
-      log "generating new openvpn user keys for #{username} for #{new_resource.instance} openvpn daemon"
-      execute "generate-openvpn-#{databag_item}" do
-        command "./pkitool #{username}"
-        cwd "#{new_resource.base_dir}/easy-rsa"
-        environment(
-          'KEY_CN'       => "#{node['openvpn']['key']['org']} #{new_resource.instance} CA",
-          'EASY_RSA'     => "#{new_resource.base_dir}/easy-rsa",
-          'KEY_CONFIG'   => "#{new_resource.base_dir}/easy-rsa/openssl.cnf",
-          'KEY_DIR'      => key_dir,
-          'CA_EXPIRE'    => node['openvpn']['key']['ca_expire'].to_s,
-          'KEY_EXPIRE'   => node['openvpn']['key']['expire'].to_s,
-          'KEY_SIZE'     => node['openvpn']['key']['size'].to_s,
-          'KEY_COUNTRY'  => node['openvpn']['key']['country'],
-          'KEY_PROVINCE' => node['openvpn']['key']['province'],
-          'KEY_CITY'     => node['openvpn']['key']['city'],
-          'KEY_ORG'      => node['openvpn']['key']['org'],
-          'KEY_EMAIL'    => node['openvpn']['key']['email']
-        )
-        notifies :run, 'execute[sync user vpn keys to s3]', :delayed
-        not_if { ::File.exist?("#{key_dir}/#{username}.crt") }
-      end
+      unless user_action == :delete
+        log "New openvpn user keys will be generated and saved to #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} databag item"
+        persisted_certs = nil
 
-      log "building #{databag_item}.json file to be uploaded to data bag #{node['opsline-openvpn']['persistence']['users_databag']}"
-      ruby_block "read keys" do
-        block do
-          ::File.open("#{key_dir}/#{databag_item}.json", "w") do |f|
-            f.puts ("{ \"id\": \"#{databag_item}\",")
-            f.puts ("\"crt\": \"#{::File.open("#{key_dir}/#{username}.crt", "r").read().gsub(/\n/,"\\n")}\",")
-            f.puts ("\"csr\": \"#{::File.open("#{key_dir}/#{username}.csr", "r").read().gsub(/\n/,"\\n")}\",")
-            f.puts ("\"key\": \"#{::File.open("#{key_dir}/#{username}.key", "r").read().gsub(/\n/,"\\n")}\"}")
+        # generate new user keys
+        log "generating new openvpn user keys for #{username} for #{new_resource.instance} openvpn daemon"
+        execute "generate-openvpn-#{databag_item}" do
+          command "./pkitool #{username}"
+          cwd "#{new_resource.base_dir}/easy-rsa"
+          environment(
+            'KEY_CN'       => "#{node['openvpn']['key']['org']} #{new_resource.instance} CA",
+            'EASY_RSA'     => "#{new_resource.base_dir}/easy-rsa",
+            'KEY_CONFIG'   => "#{new_resource.base_dir}/easy-rsa/openssl.cnf",
+            'KEY_DIR'      => key_dir,
+            'CA_EXPIRE'    => node['openvpn']['key']['ca_expire'].to_s,
+            'KEY_EXPIRE'   => node['openvpn']['key']['expire'].to_s,
+            'KEY_SIZE'     => node['openvpn']['key']['size'].to_s,
+            'KEY_COUNTRY'  => node['openvpn']['key']['country'],
+            'KEY_PROVINCE' => node['openvpn']['key']['province'],
+            'KEY_CITY'     => node['openvpn']['key']['city'],
+            'KEY_ORG'      => node['openvpn']['key']['org'],
+            'KEY_EMAIL'    => node['openvpn']['key']['email']
+          )
+          notifies :run, 'execute[sync user vpn keys to s3]', :delayed
+          not_if { ::File.exist?("#{key_dir}/#{username}.crt") }
+        end
+
+        log "building #{databag_item}.json file to be uploaded to data bag #{node['opsline-openvpn']['persistence']['users_databag']}"
+        ruby_block "read keys" do
+          block do
+            ::File.open("#{key_dir}/#{databag_item}.json", "w") do |f|
+              f.puts ("{ \"id\": \"#{databag_item}\",")
+              f.puts ("\"crt\": \"#{::File.open("#{key_dir}/#{username}.crt", "r").read().gsub(/\n/,"\\n")}\",")
+              f.puts ("\"csr\": \"#{::File.open("#{key_dir}/#{username}.csr", "r").read().gsub(/\n/,"\\n")}\",")
+              f.puts ("\"key\": \"#{::File.open("#{key_dir}/#{username}.key", "r").read().gsub(/\n/,"\\n")}\"}")
+            end
           end
         end
-      end
     
-      require 'chef/knife' # for executing below knife command to upload data bag items
+        require 'chef/knife' # for executing below knife command to upload data bag items
 
-      execute "persisting #{databag_item}.json to data bag item #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item}" do
-        command "knife data bag from file #{node['opsline-openvpn']['persistence']['users_databag']} #{key_dir}/#{databag_item}.json --secret-file /etc/chef/encrypted_data_bag_secret -c /etc/chef/client.rb -u #{client_username} -k #{client_key_file}"
-      end
+        execute "persisting #{databag_item}.json to data bag item #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item}" do
+          command "knife data bag from file #{node['opsline-openvpn']['persistence']['users_databag']} #{key_dir}/#{databag_item}.json --secret-file /etc/chef/encrypted_data_bag_secret -c /etc/chef/client.rb -u #{client_username} -k #{client_key_file}"
+        end
 
-      # delete the json file just created
-      file "#{key_dir}/#{databag_item}.json" do
-        action :delete
+        # delete the json file just created
+        file "#{key_dir}/#{databag_item}.json" do
+          action :delete
+        end
       end
     end # end of rescue block
 
-    unless persisted_certs.nil?
+    unless persisted_certs.nil? || user_action == :delete
       log "Restoring persisted user keys found in #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} databag item"
       file "#{key_dir}/#{username}.crt" do
         content persisted_certs['crt']
@@ -140,18 +143,21 @@ action :create do
         action user_action
         notifies :run, 'execute[sync user vpn keys to s3]', :delayed
       end
-    else
-      if user_action == :delete
-        %w(crt csr key).each do |ext|
-          file "#{key_dir}/#{username}.#{ext}" do
-            action user_action
-          end
-        end
+    end
 
-        # delete the persisted user keys from the data bag
-        execute "deleting persisted #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} data bag item" do
-          command "knife data bag delete #{node['opsline-openvpn']['persistence']['users_databag']} #{databag_item} --secret-file /etc/chef/encrypted_data_bag_secret  -c /etc/chef/client.rb -u #{node['opsline-openvpn']['persistence']['admin_databag_item']} -k #{client_key_file}"
+    if user_action == :delete
+      # even if we didn't have persisted certs in the data bag, delete any existing user keys/certs on the vpn server
+      log "Deleting persisted user keys from this host and #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} databag item"
+      %w(crt csr key).each do |ext|
+        file "#{key_dir}/#{username}.#{ext}" do
+          action user_action
         end
+      end
+
+      # finally, delete the persisted user keys from the data bag
+      execute "deleting persisted #{node['opsline-openvpn']['persistence']['users_databag']}:#{databag_item} data bag item" do
+        command "knife data bag delete #{node['opsline-openvpn']['persistence']['users_databag']} #{databag_item} -y -c /etc/chef/client.rb -u #{client_username} -k #{client_key_file}"
+        returns [ 0, 100 ] # return code 100 when data bag item does not exist
       end
     end
 
@@ -171,17 +177,18 @@ action :create do
     if node['opsline-openvpn']['tls_key']
       tar_cmd += " #{node['opsline-openvpn']['tls_key']}"
     end
-    execute 'create openvpn tarball for user' do
-      cwd key_dir
-      command tar_cmd
-      action :run
-      not_if { user_action == :delete } 
-    end
-    file 'delete openvpn tarball for deprecated user' do
-      name tar_file
-      action :delete
-      notifies :run, 'execute[sync user vpn keys to s3]', :delayed
-      only_if { user_action == :delete }
+    if user_action == :create
+      execute 'create openvpn tarball for user' do
+        cwd key_dir
+        command tar_cmd
+        action :run
+      end
+    else
+      file 'delete openvpn tarball for deprecated user' do
+        name tar_file
+        action :delete
+        notifies :run, 'execute[sync user vpn keys to s3]', :delayed
+      end
     end
   end
 
